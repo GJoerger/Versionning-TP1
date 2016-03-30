eb3a3169fe7a6001911f9b3791809bd5cca87060
Gregory Joerger <greg.joerger@gmail.com>
2016-03-30 12:03:34 +0200

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,5 +1,6 @@
 ### application.bat - 2016 - Gregory Joerger ###
+version = "2"
+build_hour = "11h00"
 
-echo "Copyright IMIE - 2016"
-echo "Build : 10:00"
-echo "version 1.1"
+echo "Build ${build_hour}"
+echo "version ${version}"
